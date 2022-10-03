9bf3f142d0b95a48c82ee040f66f650ef94b91d4
Nicolas vauché <hello@nicolasvauche.net>
2022-10-03 22:40:55 +0200

    Prepare for SW

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 import { sliderFactory } from './factory/slider.js'
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Check ServiceWorker
+  if ('serviceWorker' in navigator) {
+    console.log('Service Worker available')
+  }
+
   // Get Slider container
   const sliderContainer = document.getElementById('slider')
   // Get Slider Controls container
